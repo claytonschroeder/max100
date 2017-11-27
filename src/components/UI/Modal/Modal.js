bba331f14cde326f9c2431705182acd84cfdb48f
claytonschroeder <clayton.schroeder@aulmni.ubc.ca>
2017-11-27 14:55:25 -0800

    push to page from dropdown. add instructions.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,15 +2,32 @@ import React from 'react';
 
 import classes from './Modal.css';
 
+import dragSubs from '../../../assets/images/drag_subs.jpg'
+
 const modal = (props) => {
     const getBodyText = (title) => {
         switch(title){
             case 'SMARTER':
-                return(<p>Some instructions about the smarter technique</p>)
+                return(
+                    <div>
+                        <p>Some instructions about the smarter technique</p>
+                        <img src={dragSubs} alt="Drag and Drop Example"/>
+                    </div>
+                )
             case 'Swing Weighting':
-                return(<p>Some instructions about the swing weighting technique</p>)
+                return(
+                    <div>
+                        <p>Some instructions about the swing weighting technique</p>
+                        <img src={dragSubs} alt="Drag and Drop Example"/>
+                    </div>
+                )
             case 'Max 100':
-                return(<p>Some instructions about the max 100 technique</p>)
+                return(
+                    <div>
+                        <p>In the 'Max100' weighting approach, criteria weights are defined through two steps - first by RANKING the criteria and sub-criteria by importance, followed by RATING their relative importance on a 0-100 scale.The following questions will ask you to review a number of evaluation criteria for a hypothetical alternatives analysis, and provide your take on the relative importance of each criterion.</p>
+                        <img src={dragSubs} alt="Drag and Drop Example"/>
+                    </div>
+                )
             default: return null
         }
     }
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -8,16 +8,16 @@ import classes from './Home.css'
 
 class Home extends Component {
     state = {
-        name: ''
+        name: '',
+        case: '/case/max100'
     }
 
     validate = () => {
         if(this.state.name.length === 0){
             alert('Please enter your name before continuing')
         } else {
-            console.log(this.state.name)
             this.props.onNameUpdate(this.state.name);
-            this.props.history.push('/case/max100');
+            this.props.history.push(this.state.case);
         }
     }
     render () {
@@ -26,9 +26,16 @@ class Home extends Component {
                 <div className={ classes.Inner }>
                     <h2 className={ classes.Centered }>Welcome</h2>
                     <p>Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-                    <p>Many desktop publishing packages sometimes on purpose (injected humour and the like).</p>
+                    <p>Please enter your name and select the method you have been asked to use for the exercise.</p>
                     <div className={ classes.Centered }>
                         <input className={ classes.Input } type="text" value={ this.state.name } placeholder="Enter Your Name" onChange={ (event) => this.setState({name: event.target.value}) }/>
+                    </div>
+                    <div className={ classes.Centered }>
+                        <select className={ classes.Input } type="select" value={ this.state.case } onChange={ (event) => this.setState({case: event.target.value}) }>
+                            <option value='/case/max100'>Max 100</option>
+                            <option value='/case/swing-weighting'>Swing Weighting</option>
+                            <option value='/case/smarter'>SMARTER</option>
+                        </select>
                     </div>
                     <div className={ classes.Buffer }>
                         <button className={ classes.Button } disabled={ !this.state.name.length > 0 } onClick={ this.validate }>Get Started</button>
